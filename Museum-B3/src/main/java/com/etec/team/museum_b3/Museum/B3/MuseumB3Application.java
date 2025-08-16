e905cbb459114016e71123079b98b1f3813726d8
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2025-08-16 07:22:15 -0300

    feat: created the base of the project with Spring Initializr

---
+++ Museum-B3/src/main/java/com/etec/team/museum_b3/Museum/B3/MuseumB3Application.java
@@ -0,0 +1,13 @@
+package com.etec.team.museum_b3.Museum.B3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumB3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumB3Application.class, args);
+	}
+
+}
---
+++ Museum-B3/src/test/java/com/etec/team/museum_b3/Museum/B3/MuseumB3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etec.team.museum_b3.Museum.B3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumB3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
